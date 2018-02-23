a339ba4ca5da7d560ee865d8771a1185df7ba15b
Yong Zhang <18911911661@163.com>
2018-02-23 17:26:24 +0800

    update

--- src/main/java/com/iguihua/model/UserModel.java
+++
@@ -1,9 +0,0 @@
-package com.iguihua.model;
-
-import java.io.Serializable;
-
-/**
- * Created by yongzhang on 24/10/2017.
- */
-public class UserModel implements Serializable {
-}
--- src/main/java/com/iguihua/postDto/UserDto.java
+++
@@ -1,9 +0,0 @@
-package com.iguihua.postDto;
-
-import java.io.Serializable;
-
-/**
- * Created by yongzhang on 24/10/2017.
- */
-public class UserDto implements Serializable {
-}
